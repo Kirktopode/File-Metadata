b7c0adba8f88ded9813a3ad77757f7cebbb1a556
Jameson <masksofpurple@gmail.com>
2017-08-04 17:38:47 +0000

    auto-delete files after scanning them

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var fs = require('fs');
 var app = express();
 
 var port = process.env.PORT || 8080;
 app.listen(port);
-var upload = multer({ dest: './uploads/' });
+var upload = multer({ dest: './' });
 //app.use(bodyParser.urlencoded({ extended: false }));
 
 console.log("Listening on " + port);
@@ -15,10 +16,14 @@ app.use(express.static('views'));
 app.post('/upload', upload.single('upl'), function(req, res){
     console.log("POST request for /upload");
     console.log(req.file);
+    var filepath = "./" + req.file.path;
     var fileData = {
         name: req.file.originalname,
         size: req.file.size,
         date: new Date().toGMTString()
     };
+    //Immediately delete file, as we just wanted that sweet metadata
+    fs.unlinkSync(filepath);
+    
     res.end(JSON.stringify(fileData));
 });
